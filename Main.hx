8d774aab416678b4388fd2856affe72cd3493fb7
Ulfasaar <weyers.ryan@gmail.com>
2019-03-29 11:02:35 +1300

    added a really basic dict generator that just spits out a static string

--- Main.hx
+++ Main.hx
@@ -1,37 +1,48 @@
 class Main {
 
-    static function query_db(){
-        // query db multiple times with one connection
-        // use config files to store the connection details
-        // command would be query blah connection
-        // use the word close to close the connection to return to the main prompt
-        // append the db connection prompt to show still connected
-    }
+    // static function chat(){
+    //     var question_response_mapping = {};
+    //     // maps questions to responses for fun chats eg: how are you etc
+    // }
 
-    static function fill_template(){
-        // read a JSON config file 
-        // read in a template file 
-        // fill the values 
-        // spit out the result
-    }
+    // static function query_db(){
+    //     // query db multiple times with one connection
+    //     // use config files to store the connection details
+    //     // command would be query blah connection
+    //     // use the word close to close the connection to return to the main prompt
+    //     // append the db connection prompt to show still connected
+    // }
 
-    static function hide(){
-        // encrypt the thing using existing pkey or create new ones each time?
-        // used for managing secrets
-    }
+    // static function fill_template(){
+    //     // read a JSON config file 
+    //     // read in a template file 
+    //     // fill the values 
+    //     // spit out the result
+    // }
+
+    // static function hide(){
+    //     // encrypt the thing using existing pkey or create new ones each time?
+    //     // used for managing secrets
+    // }
 
     // todo add stuff for managing shared libraries and meta repos
 
     // todo unit tests!!!! BDD etc
     // to do integration tests decouple from command inputs
 
-    static function show(){
-        // unencrypt
-    }
+    // static function show(){
+    //     // unencrypt
+    // }
 
-    static function find(){
-        // finds text in a folder using silver searcher or whatever
-        // if it finds something and its encrypted and it has the primary key decrypt it
+    // static function find(){
+    //     // finds text in a folder using silver searcher or whatever
+    //     // if it finds something and its encrypted and it has the primary key decrypt it
+    // }
+
+    static function generate_random_json_blobs(){
+        // generates a n list of json blobs with different keys
+        // configurable fields num fields etc
+        return "{'cat': 'dog', 'pet': 'true'}, {'cat': 'mouse', 'pet': 'true'},  {'cat': 'moose', 'pet': 'false'}";
     }
 
     static function read(path: String): String{
@@ -141,6 +152,10 @@ class Main {
 
                 // run the app and continue prompting
                 run(path);
+            }
+
+            if(answer == 'generate random'){
+                Sys.println(generate_random_json_blobs());
             }
 
             answer = prompt();
